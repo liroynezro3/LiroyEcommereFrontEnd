b778292a06d5ca2edc5a461e37ddc651188e928b
liroynezro3@gmail.com <liroynezro3@gmail.com>
2022-09-28 15:37:49 +0300

    make it better

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import classes from "./Layout.module.css"
 const Layout = (props) => {
   return (
     <React.Fragment>
       <Header/>
       <Navbar/>
-      <main>{props.children}</main>
+      <main className={classes.main}>
+      {props.children}</main>
       <Footer/>
     </React.Fragment>
   );
